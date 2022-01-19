ae2ac764516a9317feb11af75fa502633c395249
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-01-19 21:53:22 +0530

    Create insertionSort2.java

---
+++ insertionSort2.java
@@ -0,0 +1,31 @@
+package com.basicsinjava;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void printArray(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {72,21,34,54,65,18};
+
+        //time complexity = O(n^2)     (which is not a good time complexity)
+        //Insertion sort
+        for(int i =1; i<arr.length ; i++){
+            int current = arr[i];
+            int j = i-1;
+            while(j>=0 && current < arr[j]) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+
+            //placement
+            arr[j+1] = current;
+        }
+        printArray(arr);
+    }
+}
