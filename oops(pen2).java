8045256df8d4704fa2ffedb7303c36a95b92577b
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-01-19 23:00:57 +0530

    Create oops(pen2).java

---
+++ oops(pen2).java
@@ -0,0 +1,29 @@
+package com.basicsinjava;
+
+class Pen{
+    String colour;
+    String type;  //ball,point,gel
+
+    public void write(){
+        System.out.println("Writing Something");
+    }
+    public void printColour(){
+        System.out.println(this.colour);
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        Pen pen1 = new Pen();
+        pen1.colour = "blue";
+        pen1.type = "gel";
+
+        Pen pen2 = new Pen();
+        pen2.colour = "black";
+        pen2.type = "point";
+
+        pen1.write();
+        pen2.write();
+    }
+}
