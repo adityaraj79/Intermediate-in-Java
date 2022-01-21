0f14bc15c35c33e47549a8e1e9a0ce63f20e7999
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-01-21 23:09:25 +0530

    Create hybridInheritance.java

---
+++ hybridInheritance.java
@@ -0,0 +1,25 @@
+package com.basicsinjava;
+
+class Shape{
+    public void area(){
+        System.out.println("displays area");
+    }
+}
+
+class triangle extends Shape{
+    public void area(int l, int h) {
+        System.out.println(1/2*l*h);
+    }
+}
+
+class Circle extends Shape{
+    public void area(int r){
+        System.out.println(22/7*r*r);
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
